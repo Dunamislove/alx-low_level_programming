346d634ad234e059dacece6ffd5bde067ce807df
dunamislove <dunamislove5@gmail.com>
2023-06-18 10:26:41 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - printing output withprintf
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
